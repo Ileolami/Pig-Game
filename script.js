4a24f6caa53fe3bcb8c1bed17624a336c51b0f3f
Ileolami <99335756+Ileolami@users.noreply.github.com>
2024-02-03 19:50:37 +0100

    Refactor initialization and add new game functionality

--- script.js
+++ script.js
@@ -16,10 +16,26 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+let scores, currentScore, activePlayer, playing;
+const init = ()=>{
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
+    
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    winner.textContent = '';
+
+    diceEl.classList.add('hidden');
+    document.querySelector('.player--0').classList.remove('player--winner');
+    document.querySelector('.player--1').classList.remove('player--winner');
+    document.querySelector('.player--0').classList.add('player--active');
+    document.querySelector('.player--1').classList.remove('player--active');
+}
+init();
 
 const switchPlayer = () =>{
     document.querySelector(`#current--${activePlayer}`).textContent = 0;
@@ -54,7 +70,7 @@ btnHold.addEventListener('click', () => {
         document.querySelector(`#score--${activePlayer}`).textContent = scores[activePlayer];
 
 
-        if(scores[activePlayer] >= 20){
+        if(scores[activePlayer] >= 100){
             playing = false;
             document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
             winner.textContent = `Player ${activePlayer + 1} is the winner 🎉`;
@@ -68,5 +84,7 @@ btnHold.addEventListener('click', () => {
     }
     
 })
+
+btnNew.addEventListener('click', init);
 
 
